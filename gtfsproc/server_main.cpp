35ae43d18bd60a28fc1446d81bfa3e9fa1a991b1
Daniel Brook <danb358@gmail.com>
2020-05-07 20:54:23 -0400

    Version increment for regression fix

--- gtfsproc/server_main.cpp
+++ gtfsproc/server_main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
      */
     QCoreApplication a(argc, argv);
     QCoreApplication::setApplicationName("GtfsProc");
-    QCoreApplication::setApplicationVersion("1.3c");
+    QCoreApplication::setApplicationVersion("1.3d");
 
     QTextStream console(stdout);
     QString appName = QCoreApplication::applicationName();
