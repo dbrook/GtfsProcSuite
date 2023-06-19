accea763d16fcebdd2fcfd8bfb4402d33dc7779d
Daniel Brook <danb358@gmail.com>
2023-06-19 16:31:37 -0400

    Advance to 2.0.0 release following non-regression test updates/passage.

--- gtfsproc/server_main.cpp
+++ gtfsproc/server_main.cpp
@@ -33,7 +33,7 @@ int main(int argc, char *argv[])
      */
     QCoreApplication a(argc, argv);
     QCoreApplication::setApplicationName("GtfsProc");
-    QCoreApplication::setApplicationVersion("2.0.0-alpha");
+    QCoreApplication::setApplicationVersion("2.0.0");
 
     QTextStream console(stdout);
     QString appName = QCoreApplication::applicationName();
